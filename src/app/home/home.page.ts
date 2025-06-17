ed8620c96c2b490d60d298aa1f90148aace926a3
Adarsha Majhi <adarshamajhi01@gmail.com>
2025-06-17 09:31:52 +0530

    code stacked

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-// import { InputCheckboxComponent } from '../atoms/input-checkbox/input-checkbox.component';
 // import { InputFieldComponentComponent } from '../atoms/input-field/input-field.component.component';
+// import { InputCheckboxComponent } from '../atoms/input-checkbox/input-checkbox.component';
 // import { ButtonComponent } from '../atoms/button/button.component';
 
 @Component({
